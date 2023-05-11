16be9a9dc91e16762d0fb467e911a483f2b13106
nolife101 <william.tweed@hotmail.com>
2023-05-11 22:07:03 +0100

    help from discord-py-slash, finish later

--- add_command.py
+++ add_command.py
@@ -18,7 +18,7 @@ class DiscordRequest:
 
 
     def send_http(self,
-                    modify_method:str,
+                    modify_method,
                     guild_id,
                     location
                     
@@ -28,6 +28,20 @@ class DiscordRequest:
         url+=location
         return self._discord_client.http.request(Route(modify_method,url))
     
+
+    def command_request(
+            self,
+            token,
+            modify_method,
+            interaction_id,
+            location="",
+    ):
+        url = f"/interactions/{interaction_id}/{token}/callback"
+        url+=location
+        return self._discord_client.http.request(Route(modify_method,url))
+
+
+        #FINISH LATER. TOO TIRED TO DO NOW.
 
 
     def add_slash_command(self,
@@ -46,3 +60,5 @@ class DiscordRequest:
             return self.send_http(json=new_cmdjson,modify_method="POST",guild_id=guild_id)
         return self.send_http(json=cmdjson,modify_method="POST",guild_id=guild_id)
     
+    def post_initial_response(self,_resp,interaction_id,token):
+        return self.command_request(token,"POST",interaction_id,json=_resp)
