52601332ab1e801389777c5dac4871dc649374ff
nolife101 <william.tweed@hotmail.com>
2023-05-21 16:37:50 +0100

    updated css

--- webui/host.py
+++ webui/host.py
@@ -1,10 +1,40 @@
-import gradio as gr
+import gradio as gr, pathlib
+path = pathlib.Path(__file__).parent
+blocks = gr.Blocks(css=str(path)+"/app.css")
 
+class create_webui:
 
-def placeholder(x):
-    #this will be our link to grad with the bot's code
+    def restart(self):
+        print("This is a test")
+
+    def retrieve_terminal(self):
+        return "test"
+
+    @property
+    def demo(self):
+
+        with blocks as d:
+            with gr.Tab("Bot Management"):
+                with gr.Row():
+                    btn = gr.Button("Restart bot",elem_id="restart",elem_classes="restart")
+            with gr.Tab("Terminal"):
+                terminal = gr.TextArea(label="Terminal",value="discordCBH output terminal",elem_id="terminal-background",elem_classes="terminal")    
+            
+            
+            btn.click(self.restart)
+
+            d.load(self.retrieve_terminal,None,terminal,every=1)
+
+        
+        return d
     
-    return x
+    
 
-demo = gr.Interface(placeholder,inputs="text",outputs="text")
-demo.launch()
+
+    def launch(self):
+        self.demo.queue().launch()
+    
+
+
+x = create_webui()
+x.launch()
