071b3131467172c94e1b33a6945864d4c16305f4
nolife101 <william.tweed@hotmail.com>
2023-05-22 16:06:21 +0100

    added some guild support, not all

--- webui/host.py
+++ webui/host.py
@@ -1,14 +1,25 @@
-import gradio as gr, pathlib
+import gradio as gr, pathlib,asyncio
 path = pathlib.Path(__file__).parent
 blocks = gr.Blocks(css=str(path)+"/app.css")
 
+class _guild_but:
+    def __init__(self,g):
+        self.g = g
+        self.button = None
+
 class create_webui:
+    def __init__(self,*,show_guilds:bool=True,guilds:list=None):
+        self.guilds = guilds
+        self.terminal = ""
+        self.show_guilds = show_guilds
+        
 
     def restart(self):
         print("This is a test")
 
     def retrieve_terminal(self):
-        return "test"
+        return self.terminal
+
 
     @property
     def demo(self):
@@ -19,11 +30,23 @@ class create_webui:
                     btn = gr.Button("Restart bot",elem_id="restart",elem_classes="restart")
             with gr.Tab("Terminal"):
                 terminal = gr.TextArea(label="Terminal",value="discordCBH output terminal",elem_id="terminal-background",elem_classes="terminal")    
-            
+            if self.show_guilds:
+                if self.guilds != None:
+                    guildarr = []
+                    for guild in self.guilds:
+
+                        temp = _guild_but(guild)
+                        temp.button = gr.Button(guild.name)
+                        guildarr.append(temp)
+
+                        #guilds are a pain, figure out how to implement later
+
             
             btn.click(self.restart)
 
             d.load(self.retrieve_terminal,None,terminal,every=1)
+            
+
 
         
         return d
