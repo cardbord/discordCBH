b291e2ca39ad1a27fe7d2f235ad1605e10584e1d
Hento42 <hgthomas07@outlook.com>
2023-05-27 18:22:22 +0100

    Title, Blanks fixed, Logo for no pfp guilds

--- webui/host.py
+++ webui/host.py
@@ -1,6 +1,6 @@
 import gradio as gr, pathlib,typing, discord, asyncio
 path = pathlib.Path(__file__).parent
-blocks = gr.Blocks(css=str(path)+"/app.css")
+blocks = gr.Blocks(css=str(path)+"/app.css", title="CBH webUI")
 
 class _guild_but:
     def __init__(self,g):
@@ -45,13 +45,13 @@ class create_webui:
                                             if guild.icon:
                                                 gr.ImageMask(interactive=False,value=(guild.icon.url[:len(guild.icon.url)-4]+"64"),elem_classes="imwrap",elem_id="imwrap")
                                             else:
-                                                gr.TextArea(value="no image found")
+                                                gr.ImageMask(interactive=False,value="https://media.discordapp.net/attachments/882677741441404938/1112052388031893504/R.jpg?width=662&height=662",elem_classes="imwrap",elem_id="imwrap")
                                         with gr.Column():
                                             gr.Textbox(label="Guild information",value=f"""{guild.name}
-    {guild.id}
-    {guild.member_count} members, {len(guild.members)} active
-    {guild.description if guild.description != None else ""}
-    """,elem_id="guild-background",elem_classes="guild-info")
+{guild.id}
+{guild.member_count} members, {len(guild.members)} active
+{guild.description if guild.description != None else ""}
+""",elem_id="guild-background",elem_classes="guild-info")
                                         with gr.Column():
                                             temp.button = gr.Button(">")
                                 
