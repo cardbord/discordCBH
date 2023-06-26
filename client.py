a984a8998353d4b06f05fca34b9b3c7f564fb609
WILLIAM TWEED <william.tweed@hotmail.com>
2023-06-26 10:39:03 +0100

    planned http implementation of run

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
-import discord,typing,asyncio
+import discord,typing,asyncio,testmodule
 from webui.host import create_webui
 from discord.ext import commands
 from command import DiscordCommand
+from add_command import  DiscordHTTPGateway
 class Client(discord.Client):
     def __init__(self, 
                  intents, #make intents maybe?
@@ -9,7 +10,9 @@ class Client(discord.Client):
                  ):
         super().__init__(intents,**options)
         self._current_commands = []
-
+        self.loop = asyncio.get_event_loop()
+        self.dcHTTP = DiscordHTTPGateway(None,self.loop) #assign token in run()
+        
     def create_webui_overwritten(self,show_guids:bool=True,guilds:typing.List[discord.Guild]=None):
        '''alternative creation of webui that can be used before `Client.run` or replacing a current webui
        when created, webui will not be created again in `Client.run`'''
@@ -20,21 +23,33 @@ class Client(discord.Client):
           with_webui:bool=True,
           *,
           show_guilds:bool=True,
-          guilds:typing.List[discord.Guild] = None
           ):
         token = token.strip()
-        #make login function here
-        #then we make a connect function
-        #then make guild reqs for webui (if with_webui)
+        self.dcHTTP._token = token
+        #make login function here (probably just initialise DiscordHTTPGateway and then run _create_dcHTTPgateway_session())
+        
+        #self.dc_login()?
 
+        #then we make an event loop init
 
+        #self.init_event_loop()? 
+
+        #require a connection to d.py (or at least an initialisation of it's client's http for dpy commands)
+        loop = asyncio.get_event_loop()
+        #self.start() or self.login() and self.connect()
 
         #AFTER BOT HAS STARTED, MAKE REQ FOR GUILDS AND RUN create_webui()
         
+        #(assuming bot has started and made connection to dpy:)
         if with_webui:
-          if not hasattr(self,'webui'): 
+          if not hasattr(self,'webui'):
+            
+            guilds = []
+            if show_guilds:
+              for guild in self.guilds:
+                  guilds.append(guild)
             self.webui = create_webui(show_guilds=show_guilds,guilds=guilds)
-          try: #method already run in webui.host.launch(), but we run it here to ensure gradio doesn't throw any RuntimeErrors
+          try: #method already run in webui.host.launch(), but we run it here too to ensure gradio doesn't throw any RuntimeErrors (it seems to differ for different devices)
             loop = asyncio.get_event_loop()
           except RuntimeError:
             loop = asyncio.new_event_loop()
