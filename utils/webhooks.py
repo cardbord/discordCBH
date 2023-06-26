af4af2924034a2323ec876e8ba40af2d3ffb1d2f
WILLIAM TWEED <william.tweed@hotmail.com>
2023-06-26 10:39:39 +0100

    need to remove session.close()

--- utils/webhooks.py
+++ utils/webhooks.py
@@ -28,6 +28,7 @@ exist, or may not be included in the library.
 import aiohttp, requests, typing
 from errors import HTTPError
 from ..context import Context
+from ..client import Client
 
 class webhook:
     '''Represents a discord webhook
@@ -42,8 +43,9 @@ class webhook:
 
     '''
 
-    def __init__(self,id:int,guild_id,channel_id,name,application_id,webhook_token,avatar,token):
+    def __init__(self,id:int,guild_id:int,channel_id:int,name:str,application_id:int,webhook_token:str,avatar:str,token:str,client:Client):
         self.webhook_id = id
+        self.client = client
         self.channel_id = channel_id
         self.name = name
         self.application_id = application_id
@@ -51,12 +53,12 @@ class webhook:
         self.token = webhook_token
         self.avatar = avatar
         self.client_token = token
-        
+        self._client_session = self.client.http.__httpsession
 
 
 
     async def execute(self,*,content:str,username:str=None, avatar_url:str=None):
-        client = aiohttp.ClientSession()# <- use DiscordHTTPGateway.__httpsession for this later!
+        
         if username is None:
             username = self.name
         url = f"https://discord.com/api/v9/webhooks/{self.webhook_id}/{self.token}"
@@ -65,40 +67,40 @@ class webhook:
         'username':username if username else self.name,
         'avatar_url':avatar_url if avatar_url else self.avatar
         }
-        async with client.post(url,headers=headers,json=json) as session:
+        async with self._client_session.post(url,headers=headers,json=json) as session:
             if session.status in range(200,299):
-                await client.close()
+                await self._client_session.close()
                 return
             
-            await client.close()
+            await self._client_session.close()
             raise HTTPError(f"command failed with code {session.status}")#<- make actual response messages later!
             
             
             
 
     async def edit_message(self,*,message_id:int,content:str):
-        client = aiohttp.ClientSession()
+        self._client_session = aiohttp.ClientSession()
         url = f"https://discord.com/api/v9/webhooks/{self.webhook_id}/{self.token}/messages/{message_id}"
         headers = {"Authorization": f"Bot {self.client_token}"}
         json = {'content':content}
-        async with client.patch(url,headers=headers,json=json) as session:
+        async with self._client_session.patch(url,headers=headers,json=json) as session:
             if session.status in range(200,299):
-                await client.close()
+                await self._client_session.close()
                 return
             
-            await client.close()
+            await self._client_session.close()
             raise HTTPError(f"command failed with code {session.status}")
 
     async def delete_message(self,*,message_id:int):
-        client = aiohttp.ClientSession()
+        self._client_session = aiohttp.ClientSession()
         url = f"https://discord.com/api/v9/webhooks/{self.webhook_id}/{self.token}/messages/{message_id}"
         headers = {"Authorization": f"Bot {self.client_token}"}
-        async with client.delete(url,headers=headers) as session:
+        async with self._client_session.delete(url,headers=headers) as session:
             if session.status == 204:
-                await client.close()
+                await self._client_session.close()
                 return
             
-            await client.close()
+            await self._client_session.close()
             raise HTTPError(f"command failed with code {session.status}")
             
 
@@ -108,7 +110,7 @@ class webhook:
         '''Modifies an existing webhook with a new name, avatar and channel, provided a webhook ID is given
         returns a new webhook class on success'''
         
-        client = aiohttp.ClientSession()
+        self._client_session = aiohttp.ClientSession()
         url = f"https://discord.com/api/v9/webhooks/{self.webhook_id}"
 
         params = [new_name,new_avatar,new_channel]
@@ -125,11 +127,11 @@ class webhook:
         'avatar?':params[1],
         'channel_id':params[2]
         }
-        async with client.post(url,headers=headers,json=json) as session:
+        async with self._client_session.post(url,headers=headers,json=json) as session:
             if session.status in range(200,299):
                 webhook_status = await session.text()
                 webhook_status = eval(webhook_status.replace('null','None').replace('true','True'))
-                await client.close()
+                await self._client_session.close()
                 return webhook(
                     id=webhook_status['id'],
                     type=webhook_status['type'],
@@ -144,7 +146,7 @@ class webhook:
 
 
             
-            await client.close()
+            await self._client_session.close()
             raise HTTPError(f"command failed with code {session.status}")
 
 
@@ -182,22 +184,22 @@ async def get_webhook(ctx:Context,webhook_id:int) -> typing.Optional[webhook]:
 
 
 
-async def create_webhook(ctx:Context,*,channel_id:int,webhook_name:str) -> typing.Optional[webhook]:
+async def create_webhook(ctx:Context,client:Client,*,channel_id:int,webhook_name:str) -> typing.Optional[webhook]:
 
     '''Creates a webhook from a provided channel ID and webhook name
     returns a webhook class on success'''
     
-    client = aiohttp.ClientSession()
+    client_session = client.http.__httpsession
     url = f"https://discord.com/api/v9/channels/{channel_id}/webhooks"
     headers = {"Authorization": f"Bot {ctx._token}"}
     json = {'name':webhook_name,
     'avatar?':None
     }
-    async with client.post(url,headers=headers,json=json) as session:
+    async with client_session.post(url,headers=headers,json=json) as session:
         if session.status in range(200,299):
             webhook_status = await session.text()
             webhook_status = eval(webhook_status.replace('null','None').replace('true','True'))
-            await client.close()
+            
             return webhook(
                 id=webhook_status['id'],
                 type=webhook_status['type'],
@@ -212,24 +214,24 @@ async def create_webhook(ctx:Context,*,channel_id:int,webhook_name:str) -> typin
 
 
         
-        await client.close()
+        
         raise HTTPError(f"command failed with code {session.status}")         
 
 
-async def delete_webhook(ctx:Context,*,webhook_id:int):
+async def delete_webhook(ctx:Context,client:Client,*,webhook_id:int):
 
     '''Deletes an existing webhook given the ID
-    returns a status 204 (no content) if successful (the program will manage this for you)'''
+    returns a status 204 (no content) if successful'''
 
-    client = aiohttp.ClientSession()
+    client_session = client.http.__httpsession
     url = f"https://discord.com/api/v9/webhooks/{webhook_id}"
     headers = {"Authorization": f"Bot {ctx._token}"}
-    async with client.delete(url,headers=headers) as session:
+    async with client_session.delete(url,headers=headers) as session:
         if session.status == 204:
-            await client.close()
+            
             return
         
-        await client.close()
+        
         raise HTTPError(f"command failed with code {session.status}") 
 
 
